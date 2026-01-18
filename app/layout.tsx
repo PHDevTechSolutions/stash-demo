c0574064a9b5de21e846509f0ff5d2bb89f78f49
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2026-01-18 10:27:57 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
   title: "Stash - IT Asset Management System",
   description: "Localized IT Asset Management System built with Next.js and Tailwind CSS",
   icons: {
-    icon: "/stash-mini-darkmode.png",
+    icon: "/asset.jpg",
   },
 }
 
